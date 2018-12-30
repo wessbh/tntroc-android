5c475b3c077b961a348473dfdb1792182bb204cc
Wassim Ben Hamouda <wassimbenhamouda00@gmail.com>
2018-12-30 14:27:08 +0100

    Commit in case things went crazy .. xD

--- app/src/main/java/com/example/app/androidproject/Entity/Constants.java
+++ app/src/main/java/com/example/app/androidproject/Entity/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static final String API_KEY = "api_key";
     public static final String USER_STR = "user json";
     public static int post_id = 0;
-    public static String WEBSERVICE_URL = "http://192.168.1.3";
+    public static String WEBSERVICE_URL = "http://192.168.1.8";
     public static final String WEBSERVICE_URL_SERVER = "https://tntroc.herokuapp.com";
     public static User user = new User();
     public static final String ANNONCE_IMG_PATH = WEBSERVICE_URL+"/mdw/uploadimage/uploads/annonces/";
